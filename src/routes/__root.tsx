21fa4bef2b55c63884e307fec8f5690bbe5afedf
Fernando Sanchez <f.sanchezvilela@gmail.com>
2025-08-02 22:57:37 +0200

    chore: add tailwind + react query

---
+++ src/routes/__root.tsx
@@ -0,0 +1,55 @@
+/// <reference types="vite/client" />
+import type { ReactNode } from "react";
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
+
+import appCss from "../styles/app.css?url";
+import { seo } from "~/utils/seo";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      // SEO tags
+      ...seo({
+        title: "TanStack Start Starter",
+        description: "A starter template for TanStack Start",
+        keywords: "tanstack, start, react, vite, starter",
+      }),
+    ],
+    links: [{ rel: "stylesheet", href: appCss }],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
